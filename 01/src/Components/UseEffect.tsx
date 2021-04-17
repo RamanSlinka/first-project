e8e590282825b926010ffd8b69367e2100790317
RamanSlinka <romuch1986@gmail.com>
2021-04-17 23:50:33 +0300

    add component (fale task) useEffect

--- 01/src/Components/UseEffect.tsx
+++ 01/src/Components/UseEffect.tsx
@@ -1,15 +1,21 @@
-
-
+import {useEffect, useState} from "react";
 
 
 export const  UseEffectComponent = () => {
+const [counter, setCounter] = useState(1)
+    console.log('useEffect component')
+
+    useEffect(() => {
+        console.log('useEffect start')
+        document.title = counter.toString()
 
 
+    })
     return (
         <>
         <h4>UseEffect component</h4>
 
-
+            <button onClick={() => setCounter(counter + 1)}></button>
         </>
     )
 }
