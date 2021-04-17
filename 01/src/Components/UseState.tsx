0d67c08af0eaabafe0df58617fd4702948df8d3a
RamanSlinka <romuch1986@gmail.com>
2021-04-17 23:34:11 +0300

    add component (fale task) useState(update)

--- 01/src/Components/UseState.tsx
+++ 01/src/Components/UseState.tsx
@@ -1,14 +1,30 @@
 import {useState} from "react";
 
 
+function generationData () {
+    console.log('generationData')
+    // very-very difficult counting, very very !
+    let result =  80000000001
+    return result
+}
+
+//let result = generationData()
+
 export const UseStateComponent = () => {
     console.log('UseState Component')
-const [counter,setCounter] = useState(0)
+const [counter,setCounter] = useState(generationData) // вместо инициализ. значения
+                                                    //используем результат функции
+
+
+    const changer = (state: number) => {    // создаём ф-ю, которую используем вместо
+        return state + 1                    // значения counter  в обработчике события
+            }
 
     return (
         <>
             <h4>UseState Component (updated )</h4>
-            <button onClick={() => setCounter(counter + 1)}> + </button>
+           <button onClick={() => setCounter(changer)}> + </button>
+           {/* <button onClick={() =>setCounter(counter + 1)}> + </button>*/}
             {counter}
         </>
     )
